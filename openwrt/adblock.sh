a779d6da9040c087670185bda6a581d30bb6e036
Chen <chenchenjoye@sina.com>
2023-01-07 17:28:34 +0800

    commit gh-pages

--- openwrt/adblock.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh -e
-
-[ -d /etc/dnsmasq.d ] || mkdir /etc/dnsmasq.d
-wget https://pexcn.me/daily/adblock/adblock.conf -O /tmp/adblock.conf.tmp
-mv -f /tmp/adblock.conf.tmp /etc/dnsmasq.d/adblock.conf
-
-/etc/init.d/dnsmasq restart
--- openwrt/chinadns.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-wget https://pexcn.me/daily/chnroute/chnroute.txt -O /tmp/chnroute.txt.tmp
-mv -f /tmp/chnroute.txt.tmp /etc/chinadns_chnroute.txt
-
-/etc/init.d/chinadns restart
